f860040ed6e13c371404f029f0d79032532f6dd8
4cecoder <4cecoder@users.noreply.github.com>
2024-04-18 13:43:48 -0400

    init main.go

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 // Package main github.com/4cecoder/gozure
+package main
 
-func main(){
-  }
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// handler serves the HTTP requests.
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello World!")
+	if err != nil {
+		return
+	}
+}
+
+// main sets up the HTTP server.
+func main() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Could not start server: %v", err)
+		return
+	}
+}
